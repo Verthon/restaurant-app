0bbc26de9a62beb71ece5f0bcd199908b1aab1bd
Verthon <krzysiekksordyl@gmail.com>
2018-06-28 08:40:51 -0400

    added jquery-ui, changd styling of footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 //var $ = require('node_modules/jquery');
 import $ from 'jquery';
+import 'jquery.easing';
 import 'bootstrap';
+import plugins from './plugins';
 $( document ).ready(function() {
   console.log( "ready!" );
 });
