d7454da6b14ed3b6e8ac3da8f7e7539d891c5b81
Verthon <krzysiekksordyl@gmail.com>
2018-06-29 22:11:54 -0400

    changes to navar

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ import $ from 'jquery';
 import 'jquery.easing';
 import 'bootstrap';
 import plugins from './plugins';
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".navbar-fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
 $( document ).ready(function() {
   console.log( "ready!" );
 });
