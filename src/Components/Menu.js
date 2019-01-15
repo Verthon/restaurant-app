96d77b70781b992ffd1a9a925f6432097ebaae8a
Jurr <krzysiekksordyl@gmail.com>
2019-01-14 22:33:20 -0500

    Added non elegant conditional rendering solution for async from Firebase

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -22,6 +22,7 @@ class Menu extends Component {
       snapshot.docs.forEach(doc => {
         this.setState({
           appetizers: doc.data().Appetizers,
+          desserts: doc.data().Desserts,
         });
       });
     }) ;
@@ -29,7 +30,8 @@ class Menu extends Component {
 
   render(){
     const {item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12} = menu;
-    console.log(this.state.appetizers.app1); //first log undefined, second is good
+    console.log(this.state.appetizers.app1);
+    console.log(this.state.desserts); //first log undefined, second is good
     return (
       <Fragment>
         <Navbar/>
@@ -40,18 +42,18 @@ class Menu extends Component {
               <article className="menu-section__container">
                 <h2 className="menu-section__title">{item1.category}</h2>
                 <ul className="menu-section__list">
-                  <MenuItem menu={this.state.appetizers.app1}/>
-                  <MenuItem menu={item2} />
-                  <MenuItem menu={item3} />
+                {(this.state.appetizers.app1)?<MenuItem menu={this.state.appetizers.app1}/>:null}
+                {(this.state.appetizers.app2)?<MenuItem menu={this.state.appetizers.app2}/>:null}
+                {(this.state.appetizers.app3)?<MenuItem menu={this.state.appetizers.app3}/>:null}
                 </ul>
               </article>
             </div>
             <div className="col-lg-6 col-md-12">
               <h2 className="menu-section__title">{item4.category}</h2>
               <ul className="menu-section__list">
-                <MenuItem menu={item4}/>
-                <MenuItem menu={item5} />
-                <MenuItem menu={item6} />
+              {(this.state.desserts.des1)?<MenuItem menu={this.state.desserts.des1}/>:null}
+              {(this.state.desserts.des2)?<MenuItem menu={this.state.desserts.des2}/>:null}
+              {(this.state.desserts.des3)?<MenuItem menu={this.state.desserts.des3}/>:null}
               </ul>
             </div>
   
