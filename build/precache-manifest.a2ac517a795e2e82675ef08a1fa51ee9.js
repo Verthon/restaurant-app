ba96f66a7717b90077ab239548c73705e82f7ee8
Jurr <krzysiekksordyl@gmail.com>
2019-01-18 20:23:45 -0500

    first build for firebase hosting

---
+++ build/precache-manifest.a2ac517a795e2e82675ef08a1fa51ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "962e0869f16dad1dcd3e",
+    "url": "/static/js/main.962e0869.chunk.js"
+  },
+  {
+    "revision": "af81483022bd9f3d10fe",
+    "url": "/static/js/1.af814830.chunk.js"
+  },
+  {
+    "revision": "962e0869f16dad1dcd3e",
+    "url": "/static/css/main.83786b81.chunk.css"
+  },
+  {
+    "revision": "af81483022bd9f3d10fe",
+    "url": "/static/css/1.a66fb422.chunk.css"
+  },
+  {
+    "revision": "44240fca5f2621ea8091fd24c9fa45b7",
+    "url": "/index.html"
+  }
+];
