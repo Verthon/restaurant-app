17df195367d2b94dd8a6d7be001ed8f51f3cdc36
Verthon <krzysiekksordyl@gmail.com>
2019-05-26 22:57:32 +0200

    Updates of styling in hero section. Restructured images catalog

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,12 @@ const Header  = (props) => {
     <header className="site-header">
       {props.children}
       <div className="site-header__content">
-        <h1 className="site-header__headline heading heading--large">
-          the right ingredients for the right food
+        <h1 className="site-header__headline">
+          Alkinoos Taverna
         </h1>
+        <p className="site-header__text">
+        The right ingredients for the right food
+        </p>
         <a href="/book-table"><button className="site-header__btn">book a table</button></a>
         <a href="/menu"><button className="site-header__btn site-header__btn--reverse">see the menu</button></a>
       </div>
