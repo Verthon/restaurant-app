ed401c1e9625bd4bfbcf910d1742e79ea41d1834
Verthon <krzysiekksordyl@gmail.com>
2019-06-20 17:22:39 +0200

    Addded prettier support

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
-const Header  = (props) => {
+const Header = props => {
   return (
     <header className="site-header">
       {props.children}
       <div className="site-header__content">
-        <h1 className="site-header__headline">
-          Alkinoos Taverna
-        </h1>
+        <h1 className="site-header__headline">Alkinoos Taverna</h1>
         <p className="site-header__text">
-        The right ingredients for the right food
+          The right ingredients for the right food
         </p>
-        <a href="/book-table"><button className="site-header__btn">book a table</button></a>
-        <a href="/menu"><button className="site-header__btn site-header__btn--reverse">see the menu</button></a>
+        <a href="/book-table">
+          <button className="site-header__btn">book a table</button>
+        </a>
+        <a href="/menu">
+          <button className="site-header__btn site-header__btn--reverse">
+            see the menu
+          </button>
+        </a>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
