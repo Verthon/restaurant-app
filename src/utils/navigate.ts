81e17c863041ddade86628e49091d46843202271
Verthon <christopher.sordyl@gmail.com>
2020-08-01 21:24:27 +0200

    feat: add more descriptive types and function names to the components.

--- src/utils/navigate.ts
+++ src/utils/navigate.ts
@@ -1,3 +1,7 @@
-export const navigateTo = (history: { pathname: string; }[], name: string) => {
+import { RouteComponentProps } from "react-router-dom"
+interface PropsInterface extends RouteComponentProps<any> {
+  push: ({ pathname }: { pathname: string }) => void
+}
+export const navigateTo = (history: PropsInterface, name: string) => {
   return history.push({ pathname: name })
 }
