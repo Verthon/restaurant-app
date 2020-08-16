c20f141c8c83da31ad5d67e754d548457305638f
Verthon <christopher.sordyl@gmail.com>
2020-08-16 15:24:50 +0200

    test: add crucial E2E test including:
    - fetching menu items process
    - check login process
    - check admin bookings
    - check booking process

--- cypress/integration/navigateToBooking.test.ts
+++ cypress/integration/navigateToBooking.test.ts
@@ -1,7 +1,7 @@
 
 describe('sample test', () => {
   it('check if true is true', () => {
-    cy.visit(Cypress.config().baseUrl)
+    cy.visit(`${Cypress.config().baseUrl}`)
     cy.contains('h1', 'Alkinoos Taverna')
     cy.contains('a', 'book table').click()
   })
