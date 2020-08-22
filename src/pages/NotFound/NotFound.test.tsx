00340a1e9a78a710446169ec59862450018113b6
Verthon <christopher.sordyl@gmail.com>
2020-08-22 20:51:26 +0200

    test: fix NotFound test

--- src/pages/NotFound/NotFound.test.tsx
+++ src/pages/NotFound/NotFound.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import NotFound from './NotFound'
 import { renderWithRouter } from '../../utils/testUtils'
+import { NotFound } from '../../ui/NotFound/NotFound'
 
 test('Should render NotFound component', () => {
-  const { container } = renderWithRouter(<NotFound/>)
+  const { getByText } = renderWithRouter(<NotFound/>)
   const text = '404 not found in database'
-  expect(container.innerHTML).toMatch(text)
+  expect(getByText(text)).toBeInTheDocument();
 })
