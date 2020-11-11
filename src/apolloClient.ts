f3421d764f6849604cfec9c0fe4db2bdb08811f6
Krzysztof Sordyl <krzysztof.sordyl@tsh.io>
2020-11-11 14:32:18 +0100

    [RA-2] add config for apollo with new header

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,10 +1,13 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-import { HASURA_ENDPOINT } from './endpoint'
+import { HASURA_ENDPOINT, HASURA_GRAPHQL_ADMIN_SECRET } from './endpoint'
 
 const client = new ApolloClient({
   cache: new InMemoryCache({}),
   uri: HASURA_ENDPOINT,
+  headers: {
+    'X-hasura-admin-secret': HASURA_GRAPHQL_ADMIN_SECRET
+  }
 });
 
 export { client };
