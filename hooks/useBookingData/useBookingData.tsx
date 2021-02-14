a1abaf0af4240af167a497ae1de1309d9eb4eb5d
Verthon <christopher.sordyl@gmail.com>
2021-02-14 21:39:51 +0100

    config: add missing deps

--- hooks/useBookingData/useBookingData.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-
-import { BookingDataContext } from "context/bookingData/BookingDataContext";
-
-export const useBooingData = () => {
-  const context = useContext(BookingDataContext);
-}
