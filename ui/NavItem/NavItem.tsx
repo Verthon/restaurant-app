20bdde8ebecd86e654c83d4d708b697c951e787c
Verthon <christopher.sordyl@gmail.com>
2021-02-28 21:31:50 +0100

    bugfix: add fix for failing navbar animation and ui, add fixes for buttons behaviour on mobile view

--- ui/NavItem/NavItem.tsx
+++ ui/NavItem/NavItem.tsx
@@ -2,23 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 
 import styles from "./NavItem.module.scss";
+import { generateLink } from 'utils/helpers';
 
-type Props = { hashlink: boolean; name: string; link: string }
+type Props = { hashlink?: boolean; children: React.ReactNode; path?: string }
 
-export const NavItem = ({ hashlink, name, link }: Props) => (
+export const NavItem = ({ hashlink = false, children, path = "" }: Props) => (
   <li className={styles.item}>
-    {hashlink ? (
-      <Link data-testid="navlink" href={`#${link}`}>
+      <Link data-testid="navlink" href={generateLink({ path, isHashLink: hashlink  })}>
         <a className={styles.link}>
-          {name}
+          { children }
         </a>
       </Link>
-    ) : (
-      <Link data-testid="navlink" href={`/${link}`}>
-        <a className={styles.link}>
-          {name}
-        </a>
-      </Link>
-    )}
   </li>
 )
