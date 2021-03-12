3f080b0997104246a4b5fa670b93f9a01277f2c2
Verthon <christopher.sordyl@gmail.com>
2021-03-12 22:23:14 +0100

    refactor: move logic away from review-booking site

--- components/ReviewBookingForm/ReviewBookingForm.tsx
+++ components/ReviewBookingForm/ReviewBookingForm.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React from "react";
+import dayjs from "dayjs";
+import emailjs from "emailjs-com";
+import { gql } from "@apollo/client";
 
-export const ReviewBookingForm = () => {
+import { ERROR_MSG } from "constants/messages";
+import { client } from "lib/apollo/apolloClient";
+import { Button } from "ui/Button/Button";
+import { useBookingState } from "hooks/useBooking/useBooking";
+import { BookingVariables, Props } from "./ReviewBookingForm.types";
+import Form from "components/Form";
+import { getEmailActionUrl } from "utils/helpers";
+import { showErrorNotification } from "utils/notification";
+
+const ADD_BOOKING = gql`
+  mutation(
+    $email: String!
+    $name: String!
+    $date: timestamptz!
+    $guests: smallint!
+  ) {
+    insert_bookings(
+      objects: { email: $email, name: $name, date: $date, guests: $guests }
+    ) {
+      affected_rows
+      returning {
+        id
+      }
+    }
+  }
+`;
+
+const UPDATE_BOOKING = gql`
+  mutation($id: Int!) {
+    update_bookings(_set: { confirmed: true }, where: { id: { _eq: $id } }) {
+      affected_rows
+    }
+  }
+`;
+
+export const ReviewBookingForm = ({ handleEdit, toggleModal, editable = false }: Props) => {
+  const booking = useBookingState();
+  const [loading, setLoading] = React.useState(false);
 
   const addBooking = async ({ variables }: { variables: BookingVariables }) => {
     return client.mutate({ mutation: ADD_BOOKING, variables });
+  };
+
+  const updateBooking = async ({
+    variables,
+  }: {
+    variables: BookingVariables;
+  }) => {
+    return client.mutate({ mutation: UPDATE_BOOKING, variables });
   };
 
   const handleEmailSend = async (id: number) => {
@@ -57,25 +105,48 @@ export const ReviewBookingForm = () => {
     }
   };
 
-
-  return (
-    <form onSubmit={handleBookingSubmit}>
-              <Button
-                variant="transparent"
-                size="regular"
-                type="button"
-                onClick={handleBookingEdit}
-              >
-                Edit booking
-              </Button>
+  if (editable) {
+    return ( <>
+      <div className="review-booking__form">
+            <Form
+              booking={booking}
+              handleSubmit={handleBookingSubmit}
+              submitBtn={false}
+              cssClass="form--edit"
+              action={getEmailActionUrl(booking.email)}
+              withBookingDesc={true}
+            />
+          </div>
+          <footer className="review-booking__footer review-booking__footer--edit">
+            <form onSubmit={handleBookingSubmit}>
               <Button
                 variant="light"
                 size="regular"
                 type="submit"
-                loading={false}
+                loading={loading}
               >
                 Confirm Booking
               </Button>
             </form>
+          </footer>
+    </>
+
+    )
+  }
+
+  return (
+    <form onSubmit={handleBookingSubmit}>
+      <Button
+        variant="transparent"
+        size="regular"
+        type="button"
+        onClick={handleEdit}
+      >
+        Edit booking
+      </Button>
+      <Button variant="light" size="regular" type="submit" loading={loading}>
+        Confirm Booking
+      </Button>
+    </form>
   );
 };
--- components/ReviewBookingForm/ReviewBookingForm.types.ts
+++ components/ReviewBookingForm/ReviewBookingForm.types.ts
@@ -1,3 +1,9 @@
+export type Props = {
+  handleEdit: () => void
+  toggleModal: (arg: boolean) => void
+  editable?: boolean
+}
+
 export type BookingVariables = {
   id?: number;
   email?: string;
